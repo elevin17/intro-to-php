dcc3d106ed45469bcaaa39f0556d15637ec0cb2c
elevin17 <elevinogrado@yandex.ru>
2017-06-09 13:15:45 +0300

    Create about.php

--- about.php
+++ about.php
@@ -8,3 +8,31 @@ php_EOL;
 $town='Сочи';
 $about_me='Hey there, I'm using php!';
 ?>
+<section>
+  <h1>Страница пользователя <?=$name?></h1>
+  <table>
+    <tbody>
+    <tr>
+      <td>Имя</td>
+      <td>,kf,kf<?=$name?></td>
+    </tr>
+    <tr>
+      <td>Возраст</td>
+      <td>30<?=$age?></td>
+    </tr>
+    <tr>
+      <td>Адрес электронной почты</td>
+      <td>email<?=$email?></td>
+    </tr>
+    <tr>
+      <td>Город</td>
+      <td>town<?=$town ?></td>
+    </tr>
+    <tr>
+      <td>О себе</td>
+      <td>town<?=$about_me ?></td>
+    </tr>
+    </tbody>
+  <table>
+  </section>
+
